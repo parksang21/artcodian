4aba1bae4c21231320463b83b4d5729f4caf7b43
박상욱 <steve@steve-macbook.local>
2019-06-01 13:02:10 +0900

    add ticket models

--- artcodian/ticketing/models.py
+++ artcodian/ticketing/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Tickets(models.Model):
+
+    '''
+        identification code to check whether this
+        online ticket is validate or not
+    '''
+    id_code = models.CharField(max_length=10)
+
+    '''
+        purchased date
+    '''
+    pur_date = models.DateTimeField('date purchased')
+
+    '''
+        play title
+    '''
+    title = models.CharField(max_length=200, null=True)
