46bcd1a0fedc85f4953796b1e1ac897db1713c44
sieun-bae <qotldms130@gmail.com>
2019-06-01 14:20:36 +0900

    sample api test

--- artcodian/ticketing/views.py
+++ artcodian/ticketing/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
 
+from django.http import HttpResponse
+import requests
 # Create your views here.
+
+import json
+import xmltodict
+
+key = 'f03ae19a6bed46fea2ee0637727e134c'
+
 
 def index(request):
     return HttpResponse("asdf")
+
+def show(request):
+	response = requests.get('http://www.kopis.or.kr/openApi/restful/pblprfr/PF132236?service='+key).text
+	data = xmltodict.parse(response)
+	
+	return render(request, 'arcodian/show.html', 
+		{'s_id': data['dbs']['db']['mt20id'],'name': data['dbs']['db']['prfnm'], 'place': data['dbs']['db']['fcltynm']})
+
